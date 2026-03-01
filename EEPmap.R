0d71d9908d9e8d436e62bb7c4514735499ffaaa5
“leafyleaf4” <sallyborrowman@gmail.com>
2026-03-01 17:35:16 +0000

    not running on laptop (too computationally difficult, laptop not happy) - need to try on desktop

--- EEPmap.R
+++ EEPmap.R
@@ -7,27 +7,58 @@
 #load libraries
 library(tidyverse)
 library(readxl)
-library(maps) #not this? 
 library(osmdata) #using this 
-library(ggmap)#not this? 
 library(osmplotr) #using this 
+library(sf)
 
 
 
-#oklay that didnt work will try again 
+#create area for map 
+bbox<-get_bbox(c(-3.307356, 55.886465, -2.95, 56.00))
 
+#get all the layers 
 
-bbox<-get_bbox(c(-3.307356, 55.886465, -3.116, 56.00))
-datalanduse<- extract_osm_objects(key="landuse", value=, bbox=bbox)
-datwat<- extract_osm_objects (key = "natural", value="water", bbox = bbox)
-#datwat2<- extract_osm_objects (key = "natural", value="coastline", bbox = bbox)
+#landuse
+landq <- opq(bbox = bbox) |>
+  add_osm_feature(key = "landuse", value = c("grass", "meadow", "forest", "farmland", "recreation_ground"))
+datalanduse <- osmdata_sf(landq)$osm_polygons
+
+#water
+watq<- opq(bbox = bbox) |>
+  add_osm_feature(key = "natural", value =c("water", "coastline"))
+datwat<- osmdata_sf(watq)$osm_polygons
+
+#woods
 datwood<- extract_osm_objects (key = "natural", value="wood", bbox = bbox)
 
-map<- osm_basemap(bbox = bbox, bg = "white")
-map<- add_osm_objects (map, datalanduse, col = "gray40")
+#roads
+roadq<-opq(bbox=bbox) |>
+  add_osm_feature(key = "highway", value = c("motorway", "primary", "secondary", "tertiary", "residential"))
+datroads <- osmdata_sf(roadq)$osm_polygons
+
+#buildings 
+datbuildings <- extract_osm_objects(key = "building", bbox = bbox)
+
+
+#add the layers 
+
+#basemap
+map<- osm_basemap(bbox = bbox, bg = "blue")
+
+#water
 map<- add_osm_objects (map, datwat, col="#59B7D9")
-#map<- add_osm_objects (map, datwat2, col="#59B7D9")
-map<- add_osm_objects (map, datwood, col="green")
 
+#landuse
+map<- add_osm_objects (map, datalanduse, col = "gray40")
 
+#woods
+map <- add_osm_objects(map, datwood, col = "#238B45")
+
+#buildings 
+map <- add_osm_objects(map, datbuildings, col = "grey40")
+
+#roads
+map <- add_osm_objects(map, datroads, col = "white", lwd = 0.6)
+
+#printmap
 map
