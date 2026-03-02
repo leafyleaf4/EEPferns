b6a2e20a9c6886eaf6e1a30928aa53a049f75976
“leafyleaf4” <sallyborrowman@gmail.com>
2026-03-02 15:16:28 +0000

    more graphs and tukeys test

--- EEPdataanalysis.R
+++ EEPdataanalysis.R
@@ -9,11 +9,14 @@ library(tidyverse)
 library(readxl) 
 library(lme4) #for fancy modelling 
 library(lmerTest) #testing lmer
-library(scales) 
-library(DHARMa)
+library(scales) #not using yet
+library(DHARMa) #using this!
 library(StatisticalModels)
-library(ggeffects)
-library(performance)
+library(ggeffects) #not using yet 
+library(performance) #not usiung yet 
+library(ggthemes) #for pretty grpahs
+library(emmeans) # for tukesy test 
+
 
 
 #load data
@@ -49,6 +52,31 @@ ggplot(data=ferndata, aes(x=Substrate, y=stipelength))+
 ggplot(data=ferndata, aes(x=Substrate, y=widthlengthratio))+
   geom_boxplot()
 
+#### more pretty graphs 
+sum_table <- ferndata %>% group_by(Substrate) %>% summarise (mean_frondlength = mean(frondlength), sdev = sd(frondlength), se_length = sd(frondlength)/sqrt(n()))
+
+sum_table
+
+#graph for mean, SEM, and raw data points 
+ggplot()+
+  geom_point(data =ferndata, aes(x=Substrate , y=frondlength, col=Substrate), shape=21)+
+  geom_point(data = ferndata, aes(x=Substrate , y=frondlength, col=Substrate), stat="summary", fun="mean", size=3)+
+  geom_errorbar(data=sum_table, aes(x=Substrate, ymin=(mean_frondlength-se_length), ymax=(mean_frondlength+se_length), col=Substrate), width=0.1)+
+  scale_x_discrete(labels=c("concrete"="Artificial Rock", "rock"="Natural Rock", "soil"="Soil"))+
+  labs(x= "Substrate", y= "Frond Length (mm)")+
+  theme_few()+
+  theme(legend.position = "none")
+
+#same graph, but showing effect of species 
+
+ggplot()+
+  geom_point(data =ferndata, aes(x=Substrate , y=frondlength, col=Species), alpha=0.75)+
+  geom_point(data = ferndata, aes(x=Substrate , y=frondlength), stat="summary", fun="mean", size=3)+
+  geom_errorbar(data=sum_table, aes(x=Substrate, ymin=(mean_frondlength-se_length), ymax=(mean_frondlength+se_length)), width=0.1)+
+  scale_x_discrete(labels=c("concrete"="Artificial Rock", "rock"="Natural Rock", "soil"="Soil"))+
+  labs(x= "Substrate", y= "Frond Length (mm)")+
+  theme_few()
+
 
 
 #cool results looking interesting, anova time: 
@@ -76,6 +104,44 @@ anova(test1)
 #mixed effect model with species as random effect
 test2<-lmer(frondlength~Substrate+(1|Species), data=ferndata, REML=TRUE)
 anova(test2) 
-#why isnt bestie giving me a p value 
+#o okay lower f value and p but p is still rly significant 
 
 rand(test2)
+#likelyhood ratio improved model fit
+
+
+
+#need to test assumptions of model now: 
+simulateResiduals(fittedModel = test2, plot=T) #checking assumptions using DHARMA plot 
+#slightly grim unfortunately 
+
+#trying again but logging 
+ferndata<- ferndata %>% mutate(log_frondlength=log(frondlength))
+
+#same again but with logged frondlength 
+test3<-lmer(log_frondlength~Substrate+(1|Species), data=ferndata, REML=TRUE)
+anova(test3) 
+#p and f value stronger than before 
+rand(test3)
+#not quite as good fit but still works well
+summary(test3)
+#DHARMA time 
+simulateResiduals(fittedModel = test3, plot=T)
+#DHARMA LESS BAD :DDDDD
+#THIS IS THE HYPE ONE WE WANT TO GO WITH THISSSS!!!! YIPEEE (as long as things dont change when we fix species)
+
+#tukeys test time: 
+emmeans(test3, list(pairwise~Substrate), adjust ="tukey")
+
+
+
+
+
+
+
+
+
+
+
+
+
